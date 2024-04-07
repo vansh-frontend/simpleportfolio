00d3e552331ae1ac3c40b0d7164b1b7b606af305
vansh020 <vanshdhalor04@gmail.com>
2024-04-07 16:52:20 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,31 @@
+
+var typed = new Typed('#element', {
+  strings: ['Frontend Developer', 'JavaScript Developer', 'Web Designer','Sql Programmer'],
+  typeSpeed: 50,
+  loop: true
+});
+
+
+
+
+
+function show(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+  sidebar.style.width = '73%';
+  
+}
+
+function hide(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
+
+
+ function hideSidebar() {
+var sidebar = document.querySelector('.sidebar');
+sidebar.style.display = 'none';
+
+
+}
+
